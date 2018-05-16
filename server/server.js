5bb31bb595f87fda8f802e0435b91cf378c1c67e
Erick Lui <erickjin.lui@gmail.com>
2018-05-15 17:11:18 -0700

    added put post and delete end points

--- server/server.js
+++ server/server.js
@@ -26,21 +26,8 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  const result = Joi.validate(req.body, schema);
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
-
-  if (!req.body.name || req.body.name.length < 3) {
-    res.status(400).send('Name is required and should be at least 3 characters')
-    return;
-  }
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -49,6 +36,50 @@ app.post('/api/courses', (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+  // Look up the course
+  // If not exist, return 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('Course does not exist');
+
+  const { error } = validateCourse(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Update course
+  course.name = req.body.name;
+  // Return the updated course
+  res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+  // Look up the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  // Not exist, return 404
+  if (!course) return res.status(404).send('Course ID does not exist');
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  // Return the same course
+  res.status(200).send(course);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('Course ID does not exist');
+  res.status(200).send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
+  return Joi.validate(course, schema);
+}
 
 // check if PORT is already set, otherwise default to 8080
 const port = process.env.PORT || 8080
